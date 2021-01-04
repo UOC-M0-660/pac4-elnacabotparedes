e40c094727b77cb41b0f6381d4c8905b66b61a91
elnacabotparedes <elnacabot@uoc.edu>
2021-01-04 13:00:25 +0100

    Update TwitchApiDataSource.kt

--- app/src/main/java/edu/uoc/pac4/data/TwitchApiDataSource.kt
+++ app/src/main/java/edu/uoc/pac4/data/TwitchApiDataSource.kt
@@ -1,10 +1,15 @@
 package edu.uoc.pac4.data
 
 import android.util.Log
+import edu.uoc.pac4.data.oauth.OAuthTokensResponse
+import edu.uoc.pac4.data.streams.StreamsResponse
+import edu.uoc.pac4.data.user.User
 import io.ktor.client.HttpClient
 
 
-class TwitchApiDataSource()
-{
-    private val httpClient: HttpClient = HttpClient()
+interface TwitchApiDataSource {
+    suspend fun getTokens(authorizationCode: String): OAuthTokensResponse?
+    suspend fun getStreams(cursor: String? = null): StreamsResponse?
+    suspend fun getUser(): User?
+    suspend fun updateUserDescription(description: String): User?
 }
